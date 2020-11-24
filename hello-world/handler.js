d31c57241cdc3b476af5a25fb631d08cbbea781e
soroushMosaddad99 <soroush.msd99@gmail.com>
2020-11-24 15:14:39 +1100

    hello world

--- hello-world/handler.js
+++ hello-world/handler.js
@@ -1,18 +1,35 @@
+// 'use strict';
+
+// module.exports.hello = async event => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: 'Go Serverless v1.0! Your function executed successfully!',
+//         input: event,
+//       },
+//       null,
+//       2
+//     ),
+//   };
+
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+// };
+
+
 'use strict';
 
-module.exports.hello = async event => {
-  return {
+module.exports.hello = function(event, context, callback) {
+  console.log(event); // Contains incoming request data (e.g., query params, headers and more)
+
+  const response = {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      'x-custom-header': 'My Header Value',
+    },
+    body: JSON.stringify({ message: 'Hello World!' }),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  callback(null, response);
 };
